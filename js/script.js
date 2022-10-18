5041e74459bcaf954d8fb42a4a6c1b74057531b3
KarimPucci <kizpucci@gmail.com>
2022-10-18 16:07:26 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -4,4 +4,21 @@ Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi dell
 - Ricordiamoci di inizializzare la variabile di contatore prima dell’inizio del ciclo while
 - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all’interno ciclo while*/
 
-const alimenti = [];
+//----FOR----//
+const alimenti = ["nutella", "frutta", "latte", "pasta", "acqua"];
+let length = alimenti.length;
+
+for(let i = 0; i < length; i++){
+  console.log(alimenti[i]);
+}
+
+
+//----WHILE----//
+const alimenti = ["nutella", "frutta", "latte", "pasta", "acqua"];
+let length = alimenti.length;
+let i=0;
+
+while(i<length){
+  console.log(alimenti[i]);
+  i++;
+}
